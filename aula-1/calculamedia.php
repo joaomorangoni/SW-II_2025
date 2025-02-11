b6b41d5b6577977ade55abd62374ca38416071ad
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2025-02-11 12:47:26 -0300

    Add files via upload

---
+++ aula-1/calculamedia.php
@@ -0,0 +1,16 @@
+<?php 
+    $nota1 = "6";
+    $nota2 = "2";
+    $nota3 = "8";
+
+    $media = ($nota1 + $nota2 + $nota3) / 3 ;
+
+    echo "Nota 1: " . $nota1 . "<br>";
+    echo "Nota 2: " . $nota2 . "<br>";
+    echo "Nota 3: " . $nota3 . "<br>";
+
+    if ($media >= 5){
+        echo "Media: " . 
+    }
+
+?>
---
+++ aula-1/pagina.php
@@ -0,0 +1,1 @@
+<?php echo "Bem vindo, João Pedro! "?>
